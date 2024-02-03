17da2e89679c6f25dfae8b92164e3d353c73d57d
TetianaL <4215654@gmail.com>
2024-02-03 21:58:19 +0200

    1.1.2

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ import '@fontsource/roboto/700.css';
 import { persistor, store } from '../src/redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook-new">
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook-new">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
